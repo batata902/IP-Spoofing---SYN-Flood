fb36d6456157923d5381a9ce5d204ac3443b6241
JeRM <79025708+batata902@users.noreply.github.com>
2026-08-29 10:38:39 -0300

    Implement Flask app with TCP packet sniffing

---
+++ target/app.py
@@ -0,0 +1,83 @@
+from flask import Flask, request
+from scapy.all import TCP, sniff, IP
+import threading
+import sqlite3
+from contextlib import contextmanager
+
+
+app = Flask(__name__)
+
+
+@contextmanager
+def db():
+	conn = sqlite3.connect('database.db')
+	cur = conn.cursor()
+
+	try:
+		yield conn, cur
+	finally:
+		conn.close()
+
+
+lock = threading.Lock()
+
+PACKAGE_COUNTER: int = 0
+
+
+def process_tcp(package):
+	global PACKAGE_COUNTER
+
+	with lock:
+		dont_block = True
+		if package.haslayer(IP):
+			dont_block = check_ip(package[IP].src)
+
+		if dont_block:
+			print (f'[+] TCP Package -> ', package.summary())
+			PACKAGE_COUNTER += 1
+			print (f'INFO \tContador atual: {PACKAGE_COUNTER}')
+
+
+def sniff_wire():
+	sniff(filter='tcp port 8080', prn=process_tcp)
+
+
+
+def check_ip(ip_addr: str) -> bool:
+	with db() as (conn, cur):
+		query = cur.execute('SELECT recorrencias FROM blacklist WHERE ip=?;', (ip_addr,)).fetchone()
+
+		if not query:
+			cur.execute('INSERT INTO blacklist(ip, recorrencias) VALUES (?, 1)', (ip_addr,))
+			conn.commit()
+			return True
+
+		cur.execute('UPDATE blacklist SET recorrencias=(recorrencias + 1) WHERE ip = ?;', (ip_addr,))
+		conn.commit()
+
+		if query[0] > 100:
+			return False
+
+	return True
+
+@app.route('/', methods=['GET'])
+def index():
+	if PACKAGE_COUNTER >= 30000:
+		return '', 500
+
+	if not check_ip(request.remote_addr):
+		return '<h1>BLOQUEADO</h1>', 400
+
+	return '<h1>Site maneiro</h1>'
+
+
+if __name__ == '__main__':
+	conn = sqlite3.connect('database.db')
+	conn.executescript(open('schema.sql', 'r').read())
+
+	conn.close()
+
+	t = threading.Thread(target=sniff_wire, daemon=True)
+	t.start()
+
+	app.run(host='0.0.0.0', port=8080)
